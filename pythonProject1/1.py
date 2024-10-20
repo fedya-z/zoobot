4654305094b0b4c21bb90cc735f1cb8d83d96df0
Zakharcev Fedor <zaxarvev.f@gmail.com>
2024-10-20 19:41:34 +0300

    del

--- pythonProject1/1.py
+++
@@ -1,7 +0,0 @@
-def reverse_str(string):
-   if len(string) == 0:
-       return ''
-   else:
-       return string[-1] + reverse_str(string[:-1])
-
-reverse_str('test')
--- pythonProject1/catttt.py
+++
@@ -1,1 +0,0 @@
-from Cat import Cat
